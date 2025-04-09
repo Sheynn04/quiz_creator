b13d715c33df91cdfe03227b214ccc2bbc942c6f
ycz <yczaubhreydy@gmail.com>
2025-04-09 11:33:53 +0800

    pseudocode

--- quiz_text_file_creator.py
+++ quiz_text_file_creator.py
@@ -1,1 +1,5 @@
-print('hello')
+# 1. Ask for the user to input the tile of the quiz. 
+
+# 2. Create a loop that asks the user to input the questions, choices and answers to each question. 
+
+# 3. Compile the inputs into one text file. 
