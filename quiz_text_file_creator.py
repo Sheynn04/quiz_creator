621b1b82c91f95dedf1fe9aecba5246a01ccf948
ycz <yczaubhreydy@gmail.com>
2025-04-09 11:40:39 +0800

    input quantity of items

--- quiz_text_file_creator.py
+++ quiz_text_file_creator.py
@@ -2,7 +2,13 @@
 quiz_title = input("What is the title of this quiz?: ")
 
 # 2. Ask how many items this quiz has.
-
+while True:
+    try:
+        item_quantity = int(input("How many items does this quiz have?: "))
+        break
+    except ValueError:
+        print("Please input a proper value!")
+        continue
 # 2. Create a loop that asks the user to input the questions, choices and answers to each question. 
 
 
