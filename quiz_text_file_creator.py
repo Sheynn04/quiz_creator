aaac108d2a87bf74923925ada8a937684a1ede34
ycz <yczaubhreydy@gmail.com>
2025-04-09 11:37:17 +0800

    input quiz title

--- quiz_text_file_creator.py
+++ quiz_text_file_creator.py
@@ -1,5 +1,9 @@
 # 1. Ask for the user to input the tile of the quiz. 
+quiz_title = input("What is the title of this quiz?: ")
+
+# 2. Ask how many items this quiz has.
 
 # 2. Create a loop that asks the user to input the questions, choices and answers to each question. 
+
 
 # 3. Compile the inputs into one text file. 
