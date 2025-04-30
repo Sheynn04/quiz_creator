cd9c8e5e9b9440b40c6e3dcf7d976d5f6685fa4d
ycz <yczaubhreydy@gmail.com>
2025-04-30 18:31:28 +0800

    question output definition

--- quiz_randomizer.py
+++ quiz_randomizer.py
@@ -1,5 +1,32 @@
-# 1. Import random to randomize the questions.
-# 2. Declare a temporary storage for each line. 
-# 3. Access each line.
-# 4. Shuffle the questions.
-# 5. Ask the questions. 
+import random
+
+# 1. Define how each question would output.
+
+def questions_output(quiz_file):
+    with open("quiz_creator.txt", "r") as quiz_file:
+        lines = quiz_file.readlines()
+    # Create a temporary storage for the datas that will be used.
+    individual_ques = []
+    questions = ""
+    choices = []
+    answer = ""
+
+    for line in lines:
+        if line.startswith("Question"):
+            questions = line.split(":",1).strip()
+            choices = []
+
+        elif line.startswith("Option"):
+            choices.append(line.split(":").strip())
+
+        elif line.startswith("Answer"):
+            answer = line.split(":").strip()
+            
+            # This line stores all the data of a question in one variable.
+            individual_ques.append({
+            "question": questions,
+            "choices": choices,
+            "answer": answer
+        })
+            
+    return individual_ques
